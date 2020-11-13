c251fc1037e0b101bbbc3739230678d17ea1cd26
Emilo <emilio.furnari87@gmail.com>
2020-11-13 17:41:57 +0100

    completata generazione biglietto

--- js/myscript.js
+++ js/myscript.js
@@ -17,7 +17,6 @@ element.addEventListener('click',
    console.log('questi sono i km prelevati dal dom ' + km);
    var age= document.getElementById('age').value;
    console.log('questi sono gli anni prelevati dal dom ' + age);
-
    // STABILISCO prezzi e sconto
    var prezzo = 0.21;
    console.log('prezzo per km ' + prezzo);
@@ -27,28 +26,41 @@ element.addEventListener('click',
    console.log('sconto minori ' + scontoMinori);
    var scontoOver = (prezzoNormale * 40) / 100;
    console.log('sconto over 65 ' + scontoOver);
-
+   // STABILISCO LE OFFERTE
+   var offertaMinore = "-20%"
+   var offertaOver = "-40%"
    // GESTISCO GLI SCONTI
    if (age < 18) {
      var prezzoParziale = (prezzoNormale - scontoMinori);
+     var offertaAggiunta = offertaMinore;
      console.log('prezzo minori ' + prezzoParziale);
    } else if (age > 65) {
      var prezzoParziale = (prezzoNormale - scontoOver);
+     var offertaAggiunta = offertaOver;
      console.log('prezzo Over 65 ' + prezzoParziale);
    }
    else {
      prezzoParziale;
      console.log('prezzo normale ' + prezzoParziale);
    }
-
    // PREZZO ARROTONDATO
    var prezzoFinale =  Math.round(prezzoParziale * 100)/100 + " €";
    console.log('prezzo finale ' + prezzoFinale);
 
+   // GENERO TICKET CODE
+   var ticketCode = Math.floor(Math.random() * 10000) + 90000 ;
+   // GENERO NUMERO VAGONE
+   var numeroCarrozza = Math.floor(Math.random() * 9) + 1 ;
+
+
    // STAMPO SUL DOM
    document.getElementById('ticket_name').innerHTML = name;
-   
-   document.getElementById('ticke_price').innerHTML = prezzoFinale;
+   document.getElementById('offerta').innerHTML = offertaAggiunta;
+   document.getElementById('carrozza').innerHTML = numeroCarrozza;
+   document.getElementById('ticket_code').innerHTML = ticketCode;
+   document.getElementById('ticket_price').innerHTML = prezzoFinale;
+
+
 
 
 } );
