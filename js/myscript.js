d1fd0f3103702a6e354022408772a2dc148ca0ad
Emilo <emilio.furnari87@gmail.com>
2020-11-13 15:56:22 +0100

    dati prelevati dal dom

--- js/myscript.js
+++ js/myscript.js
@@ -5,3 +5,16 @@
 // Prezzo calcolato
 // Categoria selezionata dall'utente (offerta)
 // Aggiungiamo una piccola animazione al click su "Crea" e "Annulla", se clicchiamo su annulla dobbiamo ripulire il form.
+
+
+
+var element = document.getElementById("submit");
+element.addEventListener('click',
+ function() {
+   var name = document.getElementById('name').value;
+   console.log('questo è il nome prelevato dal dom ' + name);
+   var km = document.getElementById('km').value;
+   console.log('questi sono i km prelevati dal dom ' + km);
+   var age= document.getElementById('age').value;
+   console.log('questi sono gli anni prelevati dal dom ' + age)
+} );
